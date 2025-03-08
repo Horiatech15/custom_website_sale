b1a4e542c11d9648086a6913a2f4f7d91bee0a6d
Horiatech15 <horiatech15@gmail.com>
2025-03-08 23:07:18 +0100

    update2

--- controllers/main.py
+++ controllers/main.py
@@ -2,6 +2,63 @@
 from odoo import _lt
 from odoo.http import request, route
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.payment.controllers import portal as payment_portal
+
+
+class WebsiteSalePayment(payment_portal.PaymentPortal):
+
+    def _get_mandatory_address_fields(self, country_sudo):
+        """ Return the set of common mandatory address fields.
+
+        :param res.country country_sudo: The country to use to build the set of mandatory fields.
+        :return: The set of common mandatory address field names.
+        :rtype: set
+        """
+
+        if country_sudo.code == 'DZ':
+            field_names = {'name', 'city', 'country_id', 'state_id'}
+        else:
+            field_names = super()._get_mandatory_address_fields(country_sudo)
+
+        return field_names
+
+
+    def _get_mandatory_delivery_address_fields(self, country_sudo):
+        mandatory_fields = super()._get_mandatory_delivery_address_fields(country_sudo)
+        if country_sudo.code == 'DZ':
+            mandatory_fields |= {
+                'street_name', 'street2', 'street_number', 'zip', 'city_id', 'state_id', 'country_id'
+            }
+            mandatory_fields -= {'street', 'street2', 'city', 'city_id', 'zip', 'phone', 'street_number', 'street_name'}  # Brazil uses the base_extended_address fields added above
+        return mandatory_fields
+
+    @route(['/shop/country_info/<model("res.country"):country>'], type='json', auth="public", methods=['POST'],
+           website=True, readonly=True)
+    def shop_country_info(self, country, address_type, **kw):
+        address_fields = country.get_address_fields()
+        if address_type == 'billing':
+            required_fields = self._get_mandatory_billing_address_fields(country)
+        else:
+            required_fields = self._get_mandatory_delivery_address_fields(country)
+        country
+        if country.code == 'DZ':
+            return {
+                'fields': address_fields,
+                'zip_before_city': False,
+                'states': [(st.id, st.name, st.code) for st in country.sudo().state_ids],
+                'required_fields': list(required_fields),
+            }
+        else:
+            return {
+                'fields': address_fields,
+                'zip_before_city': (
+                        'zip' in address_fields
+                        and address_fields.index('zip') < address_fields.index('city')
+                ),
+                'states': [(st.id, st.name, st.code) for st in country.sudo().state_ids],
+                'phone_code': country.phone_code,
+                'required_fields': list(required_fields),
+            }
 
 
 class CustomWebsiteSale(WebsiteSale):
@@ -26,7 +83,7 @@ class CustomWebsiteSale(WebsiteSale):
         # partner_id = request.env['res.partner'].sudo().browse(partner_id)
         if partner_sudo and extra_form_data and extra_form_data['city_id']:
             city_id = request.env['res.city'].sudo().browse(int(extra_form_data['city_id']))
-            partner_sudo.sudo().write({'city_id': city_id.id, 'street': city_id.name, 'city': city_id.name})
+            partner_sudo.sudo().write({'city_id': city_id.id, 'city': city_id.name})
 
         return shop_address
 
@@ -55,8 +112,6 @@ class CustomWebsiteSale(WebsiteSale):
 
             'zip_before_city': False
         })
-
-        print('state_id--------', rendering_values)
         return rendering_values
 
     @route(
@@ -68,5 +123,4 @@ class CustomWebsiteSale(WebsiteSale):
     )
     def city_infos(self, state, **kw):
         states = request.env['res.city'].sudo().search([('state_id', '=', state.id)])
-        print('states--------', states)
         return {'state_cities': [(c.id, c.name) for c in states]}
